384ca964d579efe97d3137121863003e1716ec6a
joshuahysong <joshuahysong@192.168.1.5>
2016-09-15 20:28:50 -0400

    Fixed spacing and brackets in TIBattleSim.java

--- src/tibattlesim/Fleet.java
+++ src/tibattlesim/Fleet.java
@@ -27,34 +27,34 @@ public abstract class Fleet {
     
     //these variables are for organizing the order of the units in the ships array
     //and are assigned values 0-7
-   private int dreadnought2;
-   private int warSun2;
-   private int fighters;
-   private int carriers;
-   private int cruisers;
-   private int destroyers;
-   private int dreadnoughts;
-   private int warSun;
+    private int dreadnought2;
+    private int warSun2;
+    private int fighters;
+    private int carriers;
+    private int cruisers;
+    private int destroyers;
+    private int dreadnoughts;
+    private int warSun;
    
   
 
-   //variables for storing the amount of units present but not in battle
-   private int groundForces;
-   private int pds;
-   private int spaceDocks;
+    //variables for storing the amount of units present but not in battle
+    private int groundForces;
+    private int pds;
+    private int spaceDocks;
    
     //constructor
-   /*
-   fighters, pds, and ground forces can further be validated against carrying
-   capacity of the fleet:
+    /*
+    fighters, pds, and ground forces can further be validated against carrying
+    capacity of the fleet:
         if( fighters > warsun * 3 + carriers * 6 + spaceDocks * 3 - pds - groundForces)
             throw new IllegalArgumentException("Fleet has exceeded maximum capacity");
-   The only issue with this validation is differentiating between carried pds
-   units and active pds units. probably not worth it.
-   */
-   public Fleet(int dreadnoughts, int carriers, int cruisers, int destroyers,
+    The only issue with this validation is differentiating between carried pds
+    units and active pds units. probably not worth it.
+    */
+    public Fleet(int dreadnoughts, int carriers, int cruisers, int destroyers,
                 int fighters, int warSun, int groundForces, int pds, int spaceDocks){
-       
+
        //for testing purposes only. replace with some logic...probably
        this.dreadnought2 = 0;
        this.warSun2 = 1;
@@ -64,190 +64,190 @@ public abstract class Fleet {
        this.cruisers = 5;
        this.dreadnoughts = 6;
        this.warSun = 7;
-       
+
        if(dreadnoughts < 0 || dreadnoughts > 5)
            throw new IllegalArgumentException("Dreadnoughts must be 0-5");
        else{
            ships[this.dreadnoughts] = dreadnoughts;
            ships[this.dreadnought2] = dreadnoughts;
             }
-       
+
        if(carriers < 0 || carriers > 4)
            throw new IllegalArgumentException("Carriers must be 0-4");
        else
            ships[this.carriers] = carriers;
-       
+
        if(cruisers < 0 || cruisers > 8)
            throw new IllegalArgumentException("Cruisers must be 0-8");
        else
            ships[this.cruisers] = cruisers;
-       
+
        if(destroyers < 0 || destroyers > 8)
            throw new IllegalArgumentException("Destroyers must be 0-8");
        else
            ships[this.destroyers] = destroyers;
-       
+
        if(fighters < 0 || fighters > 10)
            throw new IllegalArgumentException("Fighters must be 0-10");
        else
            ships[this.fighters] = fighters;
-       
+
        if(warSun < 0 || warSun > 2)
            throw new IllegalArgumentException("War Suns must be 0-2");
        else{
            ships[this.warSun] = warSun;
            ships[this.warSun2] = warSun;
             }
-       
+
        if(groundForces < 0 || groundForces > 12)
            throw new IllegalArgumentException("Ground forces must be 0-12");
        else
            this.groundForces = groundForces;
-       
+
        if(pds < 0 || pds > 6)
            throw new IllegalArgumentException("PDS must be 0-6");
        else
            this.pds = pds;
-       
+
        //can there be more than 1 space dock in a system??
        if(spaceDocks < 0 || spaceDocks > 3)
            throw new IllegalArgumentException("Space Docks must be 0-3");
        else
            this.spaceDocks = spaceDocks;
-       
-       
-   }//end of constructor
-   
-   SecureRandom combatDie = new SecureRandom();//new RNG object
-   
-   //return dreadnoughts
-   public int getDreadnoughts(){
-       return ships[this.dreadnoughts];
-   }
-   
-   //return carriers
-   public int getCarriers(){
-       return ships[this.carriers];
-   }
-   
-   //return cruisers
-   public int getCruisers(){
-       return ships[this.cruisers];
-   }
-   
-   //return destroyers
-   public int getDestroyers(){
-       return ships[this.destroyers];
-   }
-   
-   //return fighters
-   public int getFighters(){
-       return ships[this.fighters];
-   }
-   
-   //return warsun
-   public int getWarSun(){
-       return ships[this.warSun];
-   }
-   
-   //return ground forces
-   public int getGroundForces(){
-       return this.groundForces;
-   }
-   
-   //return pds
-   public int getPDS(){
-       return this.pds;
-   }
-   
-   //return space docks
-   public int getSpaceDocks(){
-       return this.spaceDocks;
-   }
-   
-   //return total ships in ships array
-   public int getTotalShips(){
-       int total = 0;
-       for(int unit: ships)
-           total += unit;
-       
-       return total - getWarSun()- getDreadnoughts();
-   }
-   
-   public void removeUnits(int hits){
-       for(int i = hits; i > 0; i--){
-           for(int unit = 0; unit < ships.length; unit ++){
-               if(ships[unit] > 0){
-                   ships[unit]--;
-                   break;
-               }
-           }
-       }
-   }//end of removeUnits()
-   
-   public int spaceBattle(int dreadnoughtsCV, int carriersCV, int cruisersCV,
-                        int destroyersCV, int fightersCV, int warSunCV){
-  
-       
-       int hits = 0;
-       
-       for(int i = getDreadnoughts(); i > 0; i--){
-           if(1 + combatDie.nextInt(10) >= dreadnoughtsCV)
-               hits++;
-       }
-       
-       for(int i = getCarriers(); i > 0; i--){
-           if(1 + combatDie.nextInt(10) >= carriersCV)
-               hits++;
-       }
-       
-       for(int i = getCruisers(); i > 0; i--){
-           if(1 + combatDie.nextInt(10) >= cruisersCV)
-               hits++;
-       }
-       
-       for(int i = getDestroyers(); i > 0; i--){
-           if(1 + combatDie.nextInt(10) >= destroyersCV)
-               hits++;
-       }
-       
-       for(int i = getFighters(); i > 0; i--){
-           if(1 + combatDie.nextInt(10) >= fightersCV)
-               hits++;
-       }
-       
-       for(int i = getWarSun() * 3; i > 0; i--){
-           if(1 + combatDie.nextInt(10) >= warSunCV)
-               hits++;
-       }
-       
-       return hits;
-    }//end of spaceBattle()
-   
-   public int pdsFire(int pdsCV){
-       int hits = 0;
-       
-       for(int i = getPDS(); i > 0; i--){
-           if(1 + combatDie.nextInt(10) >= pdsCV )
-               hits++;
-       }
-       
-       return hits;
-   }
-   
-   @Override
-   public String toString(){
-       return String.format("%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n",
-               "Total Ships in fleet: ", getTotalShips(),
-               "Dreadnoughts: ", getDreadnoughts(),
-               "Carriers: ", getCarriers(),
-               "Cruisers: ", getCruisers(),
-               "Destroyers: ", getDestroyers(),
-               "Fighters: ", getFighters(),
-               "War Suns: ", getWarSun(),
-               "Ground Forces: ", getGroundForces(),
-               "PDS: ", getPDS(),
-               "Space Docks: ", getSpaceDocks());
-   }
+
+
+    }//end of constructor
+
+    SecureRandom combatDie = new SecureRandom();//new RNG object
+
+    //return dreadnoughts
+    public int getDreadnoughts(){
+        return ships[this.dreadnoughts];
+    }
+
+    //return carriers
+    public int getCarriers(){
+        return ships[this.carriers];
+    }
+
+    //return cruisers
+    public int getCruisers(){
+        return ships[this.cruisers];
+    }
+
+    //return destroyers
+    public int getDestroyers(){
+        return ships[this.destroyers];
+    }
+
+    //return fighters
+    public int getFighters(){
+        return ships[this.fighters];
+    }
+
+    //return warsun
+    public int getWarSun(){
+        return ships[this.warSun];
+    }
+
+    //return ground forces
+    public int getGroundForces(){
+        return this.groundForces;
+    }
+
+    //return pds
+    public int getPDS(){
+        return this.pds;
+    }
+
+    //return space docks
+    public int getSpaceDocks(){
+        return this.spaceDocks;
+    }
+
+    //return total ships in ships array
+    public int getTotalShips(){
+        int total = 0;
+        for(int unit: ships)
+            total += unit;
+
+        return total - getWarSun()- getDreadnoughts();
+    }
+
+    public void removeUnits(int hits){
+        for(int i = hits; i > 0; i--){
+            for(int unit = 0; unit < ships.length; unit ++){
+                if(ships[unit] > 0){
+                    ships[unit]--;
+                    break;
+                }
+            }
+        }
+    }//end of removeUnits()
+
+    public int spaceBattle(int dreadnoughtsCV, int carriersCV, int cruisersCV,
+                         int destroyersCV, int fightersCV, int warSunCV){
+
+
+        int hits = 0;
+
+        for(int i = getDreadnoughts(); i > 0; i--){
+            if(1 + combatDie.nextInt(10) >= dreadnoughtsCV)
+                hits++;
+        }
+
+        for(int i = getCarriers(); i > 0; i--){
+            if(1 + combatDie.nextInt(10) >= carriersCV)
+                hits++;
+        }
+
+        for(int i = getCruisers(); i > 0; i--){
+            if(1 + combatDie.nextInt(10) >= cruisersCV)
+                hits++;
+        }
+
+        for(int i = getDestroyers(); i > 0; i--){
+            if(1 + combatDie.nextInt(10) >= destroyersCV)
+                hits++;
+        }
+
+        for(int i = getFighters(); i > 0; i--){
+            if(1 + combatDie.nextInt(10) >= fightersCV)
+                hits++;
+        }
+
+        for(int i = getWarSun() * 3; i > 0; i--){
+            if(1 + combatDie.nextInt(10) >= warSunCV)
+                hits++;
+        }
+
+        return hits;
+     }//end of spaceBattle()
+
+    public int pdsFire(int pdsCV){
+        int hits = 0;
+
+        for(int i = getPDS(); i > 0; i--){
+            if(1 + combatDie.nextInt(10) >= pdsCV )
+                hits++;
+        }
+
+        return hits;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n%s%d%n",
+                "Total Ships in fleet: ", getTotalShips(),
+                "Dreadnoughts: ", getDreadnoughts(),
+                "Carriers: ", getCarriers(),
+                "Cruisers: ", getCruisers(),
+                "Destroyers: ", getDestroyers(),
+                "Fighters: ", getFighters(),
+                "War Suns: ", getWarSun(),
+                "Ground Forces: ", getGroundForces(),
+                "PDS: ", getPDS(),
+                "Space Docks: ", getSpaceDocks());
+    }
    
 }//end of Fleet class
--- src/tibattlesim/TIBattleSim.java
+++ src/tibattlesim/TIBattleSim.java
@@ -12,11 +12,15 @@ public class TIBattleSim {
     
     public static void main(String[] args) {
     
-    for(int i = 0; i < 1000; i++){
+    for(int i = 0; i < 1000; i++) 
+    {
         FederationOfSol attacker = new FederationOfSol(1,1,1,1,1,0,0,0,0);
         FederationOfSol defender = new FederationOfSol(1,1,1,1,1,0,3,0,0);
         attacker.removeUnits(defender.rollPDSDice());
-        while(attacker.getTotalShips() > 0 && defender.getTotalShips() > 0){
+        
+        while(attacker.getTotalShips() > 0 && defender.getTotalShips() > 0)
+        {
+            
             int defenderHits = defender.rollDice();
             int attackerHits = attacker.rollDice();
             attacker.removeUnits(defenderHits);
@@ -37,7 +41,8 @@ public class TIBattleSim {
         
     }//end of main
     
-    private static String battleReport(){
+    private static String battleReport()
+    {
         return String.format("%n%s%.1f%n%s%.1f%n%s%.1f%n", "Attacker Wins: %", (float)attackerWins/10,
                             "Defender Wins: %", (float)defenderWins/10, "Tie Battle: %", (float)tie/10);
     }
